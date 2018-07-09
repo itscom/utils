96e36e4501052ec0b0430670f337e5ea6ba06e06
Angelo Alfano <angeloalfano@gmail.com>
2018-07-09 11:03:07 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# ITS Utils
+
+TBW
---
+++ src/Its/Utils/Sanitize.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ngi
+ * Date: 27/04/2018
+ * Time: 19:37
+ */
+
+namespace Its\Utils;
+
+
+class Sanitize {
+
+	static public function slugify($text)
+	{
+		// replace non letter or digits by -
+		$text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+		// transliterate
+		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+		// remove unwanted characters
+		$text = preg_replace('~[^-\w]+~', '', $text);
+
+		// trim
+		$text = trim($text, '-');
+
+		// remove duplicate -
+		$text = preg_replace('~-+~', '-', $text);
+
+		// lowercase
+		$text = strtolower($text);
+
+		if (empty($text)) {
+			return 'n-a';
+		}
+
+		return $text;
+	}
+
+	public static function codify($text) {
+		$text = Sanitize::slugify($text);
+		$text = preg_replace('/-+/', '', $text);
+		return $text;
+	}
+}
